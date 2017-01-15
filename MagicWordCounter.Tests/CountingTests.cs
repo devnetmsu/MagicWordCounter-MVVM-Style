dc868540764b22f58130c7038df0220f8b2b79a4
Evan Dixon <evanldixon@live.com>
2017-01-15 11:56:42 -0600

    Added stub projects

---
+++ MagicWordCounter.Tests/CountingTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MagicWordCounter.Tests
+{
+    [TestClass]
+    public class CountingTests
+    {
+        [TestMethod]
+        public void BasicCounting()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ExcludeArticles()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void NamesAreOneWord()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
